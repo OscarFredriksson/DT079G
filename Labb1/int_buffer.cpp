bd23bbe17187f539e07f94e8c266a5a408ee0b9c
Oscar <osfr1701@student.miun.se>
2018-11-09 17:03:29 +0100

    början på int_sorted.cpp

--- Labb1/int_buffer.cpp
+++ Labb1/int_buffer.cpp
@@ -2,30 +2,30 @@
 
 int_buffer::int_buffer(size_t size) 
     : _size(size), _ptr(new int[size])
-    {
-        puts("constructor");
-    };    //initiera _size med inargumentets värde
+{
+    puts("constructor");
+};    //initiera _size med inargumentets värde
     
 
 int_buffer::int_buffer(const int* source , size_t size)
     : _size(size), _ptr(new int[size])
 {    
     for(int i = 0; i < size; i++)   _ptr[i] = source[i];
-}
+};
 
 int_buffer::int_buffer(const int_buffer& rhs)
     : _size(rhs.size()), _ptr(new int[rhs.size()])
 {
     puts("copy constructor");
     *this = rhs;
-}
+};
 
 int_buffer::int_buffer(int_buffer&& rhs)
     : _size(rhs.size())
 {
     puts("move constructor");
     *this = std::move(rhs);
-}
+};
 
 int_buffer & int_buffer::operator=(const int_buffer& rhs)
 {
@@ -33,7 +33,7 @@ int_buffer & int_buffer::operator=(const int_buffer& rhs)
         _ptr[i] = *(rhs.begin() + i);
     
     return *this;
-}
+};
 
 int_buffer & int_buffer::operator=(int_buffer&& rhs)
 {
@@ -41,36 +41,36 @@ int_buffer & int_buffer::operator=(int_buffer&& rhs)
     rhs._ptr = nullptr;
 
     return *this;
-}
+};
 
 size_t int_buffer::size() const
 {
     return _size;
-}
+};
 
 int_buffer::~int_buffer()
 {
     puts("destructor");
     delete[] _ptr;
-}
+};
 
 int* int_buffer::begin()
 {
     return _ptr;
-}
+};
 
 int* int_buffer::end() 
 {
     return _ptr + _size;
-}
+};
 
 const int* int_buffer::begin() const
 {
     return const_cast<int*>(_ptr);
-}
+};
 
 const int* int_buffer::end() const
 {
     return _ptr + _size;
-}
+};
 
--- Labb1/int_sorted.cpp
+++ Labb1/int_sorted.cpp
@@ -1,2 +1,29 @@
 #include "int_sorted.h"
 
+int_sorted::int_sorted(const int* source, size_t size)
+    :_buffer(source, size){};
+
+size_t int_sorted::size() const
+{
+  return _buffer.size();  
+};
+
+int* int_sorted::insert(int value)
+{
+
+};
+
+const int* int_sorted::begin() const
+{
+    return _buffer.begin();
+};
+
+const int* int_sorted::end() const
+{
+    return _buffer.end();
+};
+
+int_sorted int_sorted::merge(const int_sorted& merge_with) const
+{
+
+};
