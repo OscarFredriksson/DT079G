2e448efe56cbd0881f7d67f941a695cdcdd2680e
Oscar <osfr1701@student.miun.se>
2018-11-21 10:04:16 +0100

    Skrev lite fler kommentarer

--- Labb1/int_sorted.cpp
+++ Labb1/int_sorted.cpp
@@ -7,46 +7,49 @@
 int_sorted::int_sorted(const int* source, size_t size)
     :_buffer(source, size)
 {
-    if(size <= 1)   return;
+    if(size <= 1)   return; //Om storleken är 0 eller 1 behöver den inte sorteras
 
-    int_sorted temp = sort(source, source + size);
+    //Sortera source och lägg över den sorterade int_sorted i en temporär instans
+    int_sorted temp = sort(source, source + size);  
  
-    _buffer = std::move(temp._buffer);
+    _buffer = std::move(temp._buffer);  //Flytta den temporära till denna
 }
 
 size_t int_sorted::size() const
 {
-  return _buffer.size();  
+    return _buffer.size();  //Ta storleken för buffern
 }
 
 int* int_sorted::insert(int value)
 {
-    int_buffer temp_buffer(_buffer.size()+1);
+    int_buffer temp_buffer(_buffer.size()+1);   //Skapa en temporär buffer att bygga upp den nya buffern i
 
-    int* insertpoint = temp_buffer.begin();
+    int* insertpoint = temp_buffer.begin(); //Sätter insertpunkten till den första adressen
 
-    for(const int* i = _buffer.begin(); i != _buffer.end(); i++)
+    for(const int* i = _buffer.begin(); i != _buffer.end(); i++)    //Går igenom hela buffern
     {
-        if(value < *i) break;
+        if(value < *i) break;   //När den hittar platsen för det nya värdet, avbryt loopningen
         
-        insertpoint++;
+        insertpoint++;  //Flytta insertpunkten framåt i listan
     }
     
-    *insertpoint = value;
+    *insertpoint = value;   //Sätt in det nya värdet där det ska vara
 
     const int* prev_iter = _buffer.begin();
     int* new_iter = temp_buffer.begin();
 
-    for(;prev_iter != _buffer.end(); ++prev_iter, ++new_iter)
+      
+    //Loopa igenom båda buffrarna och kopiera över alla element från den gamla buffern till temp_buffer
+    for(;prev_iter != _buffer.end(); ++prev_iter, ++new_iter)   
     {
-        if(new_iter == insertpoint) ++new_iter;
+        if(new_iter == insertpoint) ++new_iter; //När den når det insertade elementet, hoppa över detta
 
-        *new_iter = *prev_iter;
+        *new_iter = *prev_iter; //Lägg över värdet
     }
 
-    _buffer = std::move(temp_buffer); 
+    _buffer = std::move(temp_buffer);   //Byt ut buffern till den nyskapade 
 
-    return insertpoint;
+    return insertpoint; //Returnera insertpunkten
 }
 
 const int* int_sorted::begin() const
@@ -80,6 +83,7 @@ int_sorted int_sorted::merge(const int_sorted& merge_with) const
             iter2++;
         }
     }
+
     //Om det finns nått kvar i någon av buffrarna, skriv dessa till den nya
     for(;iter1 != end(); iter1++, merged_iter++) 
         *merged_iter = *iter1;
--- Labb1/main.cpp
+++ Labb1/main.cpp
@@ -5,13 +5,13 @@
 
 void f(int_buffer buf);
 
-void print_buffer(int_buffer src);
+void print_buffer(int_buffer src);  //Skriver ut alla element i en buffer
 
-void print_sorted(int_sorted src);
+void print_sorted(int_sorted src);  //Skriver ut alla element i en int_sorted
 
 void selection_sort(int* begin, int* end);
 
-void test_insert();
+void test_insert(); 
 
 void test_sorts();
 
@@ -19,11 +19,11 @@ int main()
 {
     srand(time(NULL));
 
-    //f(int_buffer(10));
+    f(int_buffer(10));
 
     //test_insert();
 
-    test_sorts();
+    //test_sorts();
 
     return 0;
 }
@@ -32,13 +32,12 @@ void f(int_buffer buf)
 {
     //"default" constructor för buf körs
 
+    //Fyll buffern med 1 - 10
     int j = 1;
-    for(int* i = buf.begin(); i != buf.end(); i++)  //Icke-konstanta begin och end funktioner körs här
-    {
+    for(int* i = buf.begin(); i != buf.end(); i++, j++)  //Icke-konstanta begin och end funktioner körs här
         *i = j;
-        j++;
-    }
 
+    //Skriv ut buffern
     for(const int* i = buf.begin(); i != buf.end(); i++)    //Icke-konstanta begin och end funktioner körs även här
         std::cout << *i << std::endl;
 
@@ -48,28 +47,38 @@ void f(int_buffer buf)
 void print_buffer(int_buffer src)
 {
     std::cout << std::endl;
-    for(int* i = src.begin(); i != src.end(); i++)
+    
+    //Går igenom alla element i src
+    for(const int* i = src.begin(); i != src.end(); i++)
         std::cout << *i << std::endl;
+
+    std::cout << std::endl;
 }
 
 void print_sorted(int_sorted src)
 {
     std::cout << std::endl;
+    
+    //Går igenom alla element i src
     for(const int* i = src.begin(); i != src.end(); i++)
         std::cout << *i << std::endl;
+    
+    std::cout << std::endl;    
 }
 
 void selection_sort(int* begin, int* end)
 {
+    //Går igenom alla element från begin till end
     for(int* i = begin; i != end; i++)
     {
-        int* min = i;
+        int* min = i;   //Spara det minsta värdet
 
-        for(int* j = i; j != end; j++)
-            if(*j < *min) 
+        //Gå igenom resten av elementen från i
+        for(int* j = i; j != end; j++)  
+            if(*j < *min)   //Om den hittar ett nytt minsta värde, spara detta
                 min = j;
   
-    std::swap(*i, *min);
+    std::swap(*i, *min);    //Placera det minsta värdet
     }
 }
 
@@ -77,26 +86,24 @@ void test_insert()
 {
     int_sorted test(nullptr, 0);
 
-    for(int i = 0; i < 200; i++)
+    for(int i = 0; i < 200; i++)    //Gör insert på 200 slumptal
         test.insert(rand() % 500);
 
-    print_sorted(test);
+    print_sorted(test); //Printa för att kolla att de är i ordning
 }
 
 void test_sorts()
 {
-    std::cout << std::endl;
+    std::cout << std::endl; //Gör lite plats
 
-    const int size = 400000;
+    const int size = 400000;    //Antal element som ska sorteras
 
-    int src[size];
-
+    //Skapa en source och fyll den med slumptal
+    int src[size]; 
     for(int i = 0; i < size; i++)
         src[i] = rand();
 
-    int_buffer to_sort(src, size);
-
-    
+    int_buffer to_sort(src, size);  //Skapa en ny buffer utifrån source
 
     //Start merge sort
     auto start = std::chrono::high_resolution_clock::now();
