7ddd3a061455ee6a04fb766d472b782d526678df
Oscar <osfr1701@student.miun.se>
2018-11-24 14:53:53 +0100

    en bit framåt

--- Labb3/main.cpp
+++ Labb3/main.cpp
@@ -1,8 +1,19 @@
-#include <iostream>
 #include "p_queue.h"
+#include <iostream>
 
 int main()
 {
+    p_queue<int> list;
+    
+    list.push(5);
+    list.push(6);
+    list.push(3);
+
+    std::cout << list.size() << std::endl;
+
+    for(int i = 0; i < list.size(); i++)
+        std::cout << list[i] << std::endl;
 
     return 0;
 }
+
--- Labb3/p_queue.h
+++ Labb3/p_queue.h
@@ -1,4 +1,6 @@
 #include <vector>
+#include <utility>
+#include <algorithm>
 
 template<class T>
 
@@ -9,25 +11,44 @@ public:
 
     T pop()
     {
-
+        T ret = list.back().first;
+        list.pop_back();
+        return ret;
     }
     
     void push(T e)
     {
+        list.push_back(std::make_pair(e, 5));
+
+        std::sort(list.begin(), list.end());
 
     }
 
     int size() const
     {
-        
+        return list.size();
     }
 
     bool empty() const
     {
+        return list.empty();
+    }
 
+    T* begin() const
+    {
+        return list.begin().first;
+    }
+
+    T* end() const
+    {
+        return list.end().first;
+    }
+
+    T& operator[](size_t pos)
+    {  
+        return list[pos].first; 
     }
 
 private:
-  
     std::vector<std::pair<T, int>> list;
-}
+};
